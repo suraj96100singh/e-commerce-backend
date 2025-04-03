05d006c14965b672ea8f4a0ae479750588460f19
suraj96100singh <suraj96100singh@gmail.com>
2025-04-03 08:37:40 +0530

    some changes

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -1,28 +1,7 @@
 <?php
 
-// namespace App\Controller;
-
-// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-// use Symfony\Component\HttpFoundation\JsonResponse;
-// use Symfony\Component\Routing\Attribute\Route;
-
-// final class ProductController extends AbstractController
-// {
-//     // #[Route('/product', name: 'app_product')]
-//     // public function index(): JsonResponse
-//     // {
-//     //     return $this->json([
-//     //         'message' => 'Welcome to your new controller!',
-//     //         'path' => 'src/Controller/ProductController.php',
-//     //     ]);
-//     // }
-
-
-// }
-
 namespace App\Controller;
 
-use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
